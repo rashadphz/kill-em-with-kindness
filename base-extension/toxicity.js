74735494cf7379c23af83a5210c335b5a8591d31
rashadphil <rashadphilizaire@gmail.com>
2021-03-28 02:12:55 -0400

    can now return toxicity json with appropriate timer

--- base-extension/toxicity.js
+++ base-extension/toxicity.js
@@ -1,18 +1,20 @@
-var timer = 0;
-var WAIT_TIME = 3;
+var start = Date.now();
+var WAIT_TIME = 2.5;
 
 var prev_text = "";
 
-var TOXIC_THRESHOLD = 0.8; //how toxic the message can be 
+var TOXIC_THRESHOLD = 0.75; //how toxic the message can be 
 
 document.onkeypress = function () {
-    timer = 0;
-    window.setInterval(check_timer, 3000);
+    start = Date.now();
+    window.setInterval(check_timer, 1000);
+    // check_timer();
 };
 
 function check_timer() {
-    timer++;
-    if (timer > WAIT_TIME){ //wait three seconds before checking toxicity
+    var delta = Date.now() - start; 
+    // console.log(delta);
+    if (delta/1000 > WAIT_TIME){ //wait three seconds before checking toxicity
         var elem = document.activeElement;
         if (elem.contentEditable && elem.textContent != prev_text){ //don't make api calls on the same message
             prev_text = elem.textContent;
@@ -23,19 +25,19 @@ function check_timer() {
 
 function toxicityRating(text){
     console.log(text);
+    let api_wrapper = `https://pacific-basin-18297.herokuapp.com/api/${text}/`;
+    fetch (api_wrapper)
+    .then(data =>{return data.json()})
+    .then(res=> {console.log(res)})
     // let api_wrapper = "https://pacific-basin-18297.herokuapp.com/api/group/";
     // fetch (api_wrapper)
-    // .then(data =>{return data.json()})
-    // .then(res=> {console.log(res)})
-    let api_wrapper = "https://pacific-basin-18297.herokuapp.com/api/group/";
-    fetch (api_wrapper)
-    .then(data =>{return data.json().attributes})
-    .then(res => {
-        var toxic_score = res.TOXICITY;
-        if (toxic_score > TOXIC_THRESHOLD){
-            alert("MESSAGE IS TOXIC");
-        }
+    // .then(data =>{return data.json().attributes})
+    // .then(res => {
+    //     var toxic_score = res.TOXICITY;
+    //     if (toxic_score > TOXIC_THRESHOLD){
+    //         alert("MESSAGE IS TOXIC");
+    //     }
 
-    })
+    // })
 
 };
